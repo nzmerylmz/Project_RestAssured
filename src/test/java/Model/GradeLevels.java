6696dbfec39e4506f0a8c1142ec7b208e55e2803
Nazım-PC <nzm.eryilmaz@gmail.com>
2022-10-11 11:12:17 +0300

    tes111-115 Last Commit

---
+++ src/test/java/Model/GradeLevels.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class GradeLevels {
+    private String name;
+    private String shortName;
+    private Boolean active;
+    private String id;
+    private String order;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+}
---
+++ src/test/java/Model/SubjectCategories.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class SubjectCategories {
+
+    private String name;
+    private String code;
+    private Boolean active;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+
+}
