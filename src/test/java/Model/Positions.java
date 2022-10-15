8933aad54248aaedd149a9fcf2c76aa4436a879e
Kullanıcı <baris_ozalpay@outlook.com>
2022-10-15 13:56:55 +0300

    TES-110

---
+++ src/test/java/Model/Positions.java
@@ -0,0 +1,63 @@
+package Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Positions {
+    private String id;
+    private String name;
+    private String shortName;
+    private String tenantId;
+    private boolean active;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Positions{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", shortName='" + shortName + '\'' +
+                ", tenantId='" + tenantId + '\'' +
+                ", active='" + active + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/PositionsTest.java
@@ -0,0 +1,187 @@
+import Model.Positions;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import io.restassured.specification.Argument;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PositionsTest {
+    Positions positions= new Positions();
+
+    Cookies cookies;
+
+    @BeforeClass
+    public void loginCampus() {
+        baseURI = "https://demo.mersys.io/";
+
+        Map<String, String> credential = new HashMap<>();
+        credential.put("username", "richfield.edu");
+        credential.put("password", "Richfield2020!");
+        credential.put("rememberMe", "true");
+
+        cookies=
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(credential)
+
+                        .when()
+                        .post("auth/login")
+
+                        .then()
+                        //.log().cookies()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies()
+        ;
+    }
+    String positionID;
+    String positionName;
+    String positionShortName;
+    String tenantId;
+    boolean active;
+    @Test
+    public void createPosition(){
+        positionName=getRandomName();
+        positionShortName=getRandomShortName();
+        Positions position= new Positions();
+        position.setName(positionName);
+        position.setShortName(positionShortName);
+        position.setTenantId("5fe0786230cc4d59295712cf");
+        position.setActive(true);
+
+        positionID=
+                given()
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .log().body()
+                        .body(position)
+                        .when()
+                        .post("school-sevice/api/employee-position")
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().jsonPath().getString("id")
+                ;
+    }
+    public String getRandomName() {
+        return RandomStringUtils.randomAlphabetic(30).toLowerCase();
+    }
+
+    public String getRandomShortName() {
+        return RandomStringUtils.randomAlphabetic(3).toLowerCase();
+    }
+
+    @Test(dependsOnMethods = "createPosition")
+    public void createPositionNegative(){
+
+        Positions position= new Positions();
+        position.setName(positionName);
+        position.setShortName(positionShortName);
+        position.setTenantId("5fe0786230cc4d59295712cf");
+        position.setActive(true);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(position)
+                .when()
+                .post("school-sevice/api/employee-position")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("The Position with Name \""+positionName+"\" already exists."))
+        ;
+    }
+    @Test(dependsOnMethods = "createPosition")
+    public void updatePosition() {
+
+        positionName = getRandomName();
+
+        Positions position = new Positions();
+        position.setId(positionID);
+        position.setName(positionName);
+        position.setShortName(positionShortName);
+        position.setTenantId("5fe0786230cc4d59295712cf");
+        position.setActive(true);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(position)
+
+                .when()
+                .put("school-sevice/api/employee-position")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(positionName))
+        ;
+    }
+    @Test(dependsOnMethods = "updatePosition")
+    public void deletePositionById() {
+        given()
+                .cookies(cookies)
+                .pathParam("positionID",positionID)
+                .when()
+                .delete("school-sevice/api/employee-position")
+                .then()
+                .log().body()
+                .statusCode(204)
+       ;
+    }
+    @Test(dependsOnMethods = "deletePositionById")
+    public void deletePositionByIdNegative() {
+        given()
+                .cookies(cookies)
+                .pathParam("positionID",positionID)
+                .log().uri()
+
+                .when()
+                .delete("school-sevice/api/employee-position")
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+    @Test(dependsOnMethods = "deletePositionById")
+    public void updatePositionNegative() {
+
+        positionName = getRandomName();
+
+        Positions position = new Positions();
+        position.setId(positionID);
+        position.setName(positionName);
+        position.setShortName(positionShortName);
+        position.setTenantId("5fe0786230cc4d59295712cf");
+        position.setActive(true);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(position)
+
+                .when()
+                .put("school-sevice/api/employee-position")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Can't find Position"))
+        ;
+    }
+
+
+
+
+
+
+}
